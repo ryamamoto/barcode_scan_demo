83a211e5041bc98b2ad051c0b6473c9ef1ccd50c
Ryouji Yamamoto <ry116192j@gmail.com>
2020-01-25 13:23:39 +0900

    バーコードスキャンの実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // PlatformExceptionで必要
 
 void main() => runApp(MyApp());
 
@@ -25,6 +27,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _barcode = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,10 +39,43 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('バーコード: '),
+            FlatButton(
+                color: Colors.blueAccent,
+                child: Text(
+                  'SCAN',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  barCodeScan();
+                }),
+            const SizedBox(height: 24.0),
+            Text('バーコード: $_barcode'),
           ],
         ),
       ),
     );
+  }
+
+  /// バーコードスキャン開始、結果取得
+  Future<void> barCodeScan() async {
+    try {
+      var code = await BarcodeScanner.scan();
+      setState(() {
+        _barcode = code;
+      });
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          _barcode = 'The user did not grant the camera permission!';
+        });
+      } else {
+        setState(() => _barcode = 'Unknown error: $e');
+      }
+    } on FormatException {
+      setState(() => _barcode =
+          'User returned using the "back"-button before scanning anything');
+    } on Exception catch (e) {
+      setState(() => _barcode = 'Unknown error: $e');
+    }
   }
 }
